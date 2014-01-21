846d1c260a05494b4202dfa65f4dd2742ebe240a
ymntt <ymntt@users.noreply.github.com>
2014-01-21 15:49:24 +0900

    Create final-06.c
    確認テスト分

---
+++ final-06.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+struct node{
+	struct node *next;
+	int num;
+};
+int main(void)
+{
+	struct node start,myNode,yourNode,*tmp;
+	start.num=0;	start.next=&myNode;
+	yourNode.num=19; yourNode.next=&start;
+	myNode.num=17; myNode.next=&yourNode;
+	tmp=&myNode;
+	while(tmp!=&start){
+		printf("%d\n",tmp->num);
+		tmp=tmp->next;
+	}
+	return 0;
+}

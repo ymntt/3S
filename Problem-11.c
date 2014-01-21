64f94aec6401ba0fc995322b826eeb4e8bca3a7a
ymntt <ymntt@users.noreply.github.com>
2014-01-21 17:04:06 +0900

    Create Problem-11.c

---
+++ Problem-11.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main(int argc,char *argv[])
+{
+	int cmp=0;
+	if(argc<2){
+	fprintf(stderr ,"Usage: %s str1 str2",argv[0]);
+	return -1;
+}
+	cmp = strcmp(argv[1],argv[2]);
+	if (cmp < 0){
+  printf("%d: %s is smaller than %s \n",cmp,argv[1],argv[2]);
+}else{
+	printf("%d: %s is laerger than %s \n",cmp,argv[1],argv[2]);
+}
+	return 0;
+}

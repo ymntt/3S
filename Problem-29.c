b60feb7b1e445dba792ba2479fba9e0cdfdb5048
ymntt <ymntt@users.noreply.github.com>
2014-01-21 17:50:22 +0900

    Create Problem-29.c

---
+++ Problem-29.c
@@ -0,0 +1,9 @@
+# Makefileにファイル名を変える必要があります
+#Makefile for the set of dl_list
+
+OBJS = dl_list_stdin.o dl_list_addNode.o
+dl_list_findNode.o dl_list_delNode.o dl_list_showNode.o
+
+  dl_list:dl_list ${OBJS}
+  gcc -o dl_list ${OBJS}
+

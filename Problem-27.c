6f47b7904290059be2ebc02ae0732c0a4e005af7
ymntt <ymntt@users.noreply.github.com>
2014-01-21 17:44:54 +0900

    Create Problem-27.c
    ファイル名について追記

---
+++ Problem-27.c
@@ -0,0 +1,18 @@
+/*
+   	File name : dl_list_del.c
+*/
+#include <stdlib.h>
+#include "dl_lish.h"
+
+struct node * findNode(struct node *start ,int number)
+{
+  struct node *tmp;
+  tmp=start->next;
+  while (tmp!=start){
+    if ((tmp->number==number) {
+	return tmo;
+      }
+      tmp=tmp->next;
+      }
+    return NULL;
+  }

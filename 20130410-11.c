e6aedda2c7f1593e0bae7e285d0ac526eb25683b
ymntt <ymntt@users.noreply.github.com>
2014-01-21 15:33:08 +0900

    Create 20130410-11.c
    確認テスト分

---
+++ 20130410-11.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(void)
+{
+  int i=2;
+  if(i==1){
+    printf("%d is one\n", i);
+  }else if(i==2){
+    printf("%d is two\n", i);
+  }else {
+    printf("% is neither one or two\n", i);
+  }
+  return 0;
+}

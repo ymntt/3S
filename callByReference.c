70ea8446879caa786061590ea388907c1c31de59
ymntt <ymntt@users.noreply.github.com>
2014-01-22 14:43:33 +0900

    Create callByReference.c

---
+++ callByReference.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void change(int *a,int *b);
+int main(void)
+{
+  int i=5,j=10;
+  change(&i,&j);
+  printf("%d %d\n",i,j);
+  return 0;
+}
+void change(int *a,int *b)
+{
+  int t;
+  t = *a;
+  *a = *b;
+  *b = t;
+  return;
+}
+
